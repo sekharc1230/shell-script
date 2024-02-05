4767214f69e6c05918f7aebaf1dedc5419d35a54
rajasekhar <bsekhar1230@gmail.com>
2024-02-05 18:03:13 +0530

    added git install script

--- installgit.sh
+++ installgit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
-	echo "you need to be root user and then execute this script"
-	exit 1
+        echo "you need to be root user and then execute this script"
+        exit 1
 fi
-yum install git -y
+sudo apt update
+sudo apt install git -y
 if [ $? -ne 0 ]; then
-	echo "git install is failure"
-	exit 1
+        echo "git install is failure"
+        exit 1
 else
-	echo "Git installation is success"
+        echo "Git installation is success"
 fi
-
